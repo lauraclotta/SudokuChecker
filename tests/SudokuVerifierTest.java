7d6b54ff53d5d5d686c1992bd2447e2fc3280cba
Laura Hertteli <lkuha20@student.oulu.fi>
2021-11-24 16:38:14 +0200

    Tests to test boundary values

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,6 +10,10 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+String d = "407369825632158947958724316825437169791586432346912758289643571573291684164875293";
+String e = "4107369825632158947958724316825437169791586432346912758289643571573291684164875293";
+String f = "4k7369825632158947958724316825437169791586432346912758289643571573291684164875293";
+
 SudokuVerifier v = new SudokuVerifier();
 
 	@Test
@@ -22,6 +26,27 @@ SudokuVerifier v = new SudokuVerifier();
 	public void testIncorrectString() {
 		int a = v.verify(i);
 		assertEquals("incorrect string", a, -2);
+		
+	}
+	
+	@Test
+	public void testIncorrectString2() {
+		int a = v.verify(d);
+		assertEquals("incorrect string", a, -1);
+		
+	}
+	
+	@Test
+	public void testIncorrectString3() {
+		int a = v.verify(e);
+		assertEquals("incorrect string", a, -1);
+		
+	}
+	
+	@Test
+	public void testIncorrectString4() {
+		int a = v.verify(f);
+		assertEquals("incorrect string", a, 1);
 		
 	}
 	
